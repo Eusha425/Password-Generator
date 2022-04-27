e1afbf7c10bf4aa0e4ecdd07bb4c81f2675ac128
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2022-04-27 23:00:49 +0600

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,14 +1,15 @@
 import random
 
-//variables
-uID="" //variable to store the password
-//variables from which random characters will be taken
+#variables
+uID="" #variable to store the password
+#variables from which random characters will be taken
 alpha=["#","@","!","$","%","^","&","*"]
 num=["1","2","3","4","5","6","7","8","9","0"]
 char=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+#to create random password
 for i in range(4):
     uID=uID+random.choice(num)+random.choice(alpha)+random.choice(char)
 
-print(uID)
-print(len(uID))
+print(uID) #password
+print(len(uID)) #password length
